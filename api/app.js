53b8f4fd94483a6c32248bc518f4b238f6cbb1ea
Fabian Luna <fabian.luna@almundo.com>
2018-10-24 15:32:39 -0500

    Fix header

--- api/app.js
+++ api/app.js
@@ -14,7 +14,7 @@ app.use( morgan('dev') )
 app.use((req, res, next) => {
     res.header('Access-Control-Allow-Origin', '*')
     res.header('Access-Control-Allow-Headers', 'Authorization, X-API-KEY, Origin, X-Requested-With, Content-Type, Accept, Access-Control-Allow-Request')
-    res.header('Access-Control-Allow-Method', 'GET, POST, PUT, OPTIONS, DELETE')
+    res.header('Access-Control-Allow-Methods', 'GET, POST, PUT, OPTIONS, DELETE')
     res.header('Allow', 'GET, POST, PUT, OPTIONS, DELETE')
 
     next()
